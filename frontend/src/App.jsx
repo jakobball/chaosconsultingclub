bdedac087585149e33251331aeb9aa19be55b8b4
Paul Schwarz <paulschwarz@mobile-210-044.wlan.uni-mannheim.de>
2025-04-23 21:15:53 +0200

    ConsultantView gezeigt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import ProjectsPage from './pages/Projects/ProjectsPage';
+import ConsultantView from './pages/ConsultantView/ConsultantView';
+import ErrorBoundary from './ErrorBoundary';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/projects" element={<ProjectsPage />} />
-        {/* Standardumleitung zur Projektseite */}
         <Route path="/" element={<Navigate to="/projects" />} />
+        <Route path="/ConsultantView" element={
+          <ErrorBoundary>
+            <ConsultantView />
+          </ErrorBoundary>
+        } />
       </Routes>
     </BrowserRouter>
   );
