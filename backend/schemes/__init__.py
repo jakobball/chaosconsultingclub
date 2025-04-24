d33245dee0d8caf3094c1adca027d55692de49a9
jakobball20 <jakobball20@gmail.com>
2025-04-24 07:01:44 +0200

    Bug fix

---
+++ backend/schemes/__init__.py
@@ -0,0 +1,7 @@
+from . import Consultant
+from . import consultant_feedback
+from . import Customer
+from . import project
+from . import project_with_customer
+from . import staffing
+from . import staffing_assignment
---
+++ backend/service/__init__.py
@@ -0,0 +1,5 @@
+from . import consultant_service
+from . import create_staffing_service
+from . import customer_service
+from . import n8nRequests
+from . import project_service
